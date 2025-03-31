2e066d05cc18c7ed8d3eb37ce5df67f5d2383b61
bhg0023 <benjamingoldberg@my.unt.edu>
2025-03-31 17:16:16 -0500

    modified:   Communications/hardware_encode.py modified:   Communications/webcam.py

--- Communications/hardware_encode.py
+++ Communications/hardware_encode.py
@@ -3,16 +3,40 @@
 # This code will encode live video into H.264 making use
 # of the Pi's Hardware Encoder (h264_omx). 
 # force all streams down to 5Mb/s
+#
+# 
+
 
 import webcam
 import cv2
+import subprocess
 
-fourcc = cv2.VideoWriter_fourcc(*'H264')
+
+cv2_out = webcam.generate_frames()
+
+
+
+command = ['ffmpeg',
+            '-i', cv2_out,
+            '-f', 'image2',
+            '-r', '30', 
+            '-vcodec', 'libx264',
+            'out.mp4'
+           ]
+
+proc = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+ffmpeg_out = proc.stdout
+print(ffmpeg_out)
+
+
+
+""" fourcc = cv2.VideoWriter_fourcc(*'H264')
 out = cv2.VideoWriter('appsrc ! videoconvert ! x264enc tune=zerolatency noise-reduction=10000 bitrate=2048 speed-preset=superfast ! rtph264pay config-interval=1 pt=96 ! udpsink host=127.0.0.1 port=5000',fourcc,cv2.CAP_PROP_FPS, (800,600),True)
 
 def send_stream():
     while webcam.cap.isOpened():
-        webcam.generate_frames()
-        out.write(webcam.generate_frames.image).tobytes()
+        frame = webcam.generate_frames()
+        out.write(webcam.generate_frames()).tobytes()
         print(out)
 
+ """
--- Communications/webcam.py
+++ Communications/webcam.py
@@ -8,9 +8,13 @@
 # As of right now, a flask server is created to be able to moniter the
 # webcam feed in the browser.
 
-# IP address of Ben's pi: 192.168.0.103
+
+# type: "http://<IP ADDRESS OF PI>:5000/video_feed" into your web browser to view
+
+
 import cv2
 from flask import Flask, Response
+import time
 
 app = Flask(__name__)
 cam = cv2.VideoCapture(0,cv2.CAP_V4L2)  #pipline set to CAP_V4L2 to avoid pipeline error
@@ -21,8 +25,20 @@ def generate_frames():
         result, image = cam.read()
         ret, buffer = cv2.imencode('.jpg', image)
         image = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
+        return image
+        # yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
+    
+
+
+
+
+""" for i in range (0, 1):
+    out = generate_frames()
+    print(out)
+    time.sleep(0.001)
+
+
+print("End of webcam read") """
 
 """ @app.route('/video_feed')
 def video_feed():
@@ -31,3 +47,4 @@ def video_feed():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
  """
+
