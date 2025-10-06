cd2dffd68ce675be1b0dd4ab802ecec2bbe25108
julianb8 <flores.julian60@gmail.com>
2025-10-06 16:19:33 -0500

    Created a controls folder and began writing a basic script to connect to the pixhawk

---
+++ Controls/send_receive_test.py
@@ -0,0 +1,6 @@
+from pymavlink import mavutil
+
+# Sets up communication interface between the PixHawk and the Jetson
+# Using UART with BAUD rate: 115200
+connection = mavutil.mavlink_connection('/dev/ttyTHS1', 115200)
+connection.wait_heartbeat()
