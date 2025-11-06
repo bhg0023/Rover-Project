de4b89ed4d5bf7e1c7d1a120ec03aff5857d7840
JuliaanB8 <92193516+JuliaanB8@users.noreply.github.com>
2025-11-05 18:02:47 -0600

    Update send_receive_test.py

--- Controls/send_receive_test.py
+++ Controls/send_receive_test.py
@@ -7,3 +7,24 @@ connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (connection.target_system, connection.target_component))
 
 
+message = connection.mav.command_long_encode(
+        connection.target_system,  # Target system ID
+        connection.target_component,  # Target component ID
+        mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,  # ID of command to send
+        0,  # Confirmation
+        mavutil.mavlink.MAVLINK_MSG_ID_BATTERY_STATUS,  # param1: Message ID to be streamed
+        1000000, # param2: Interval in microseconds
+        0,       # param3 (unused)
+        0,       # param4 (unused)
+        0,       # param5 (unused)
+        0,       # param5 (unused)
+        0        # param6 (unused)
+        )
+
+connection.mav.send(message)
+
+response = connection.recv_match(type='COMMAND_ACK', blocking=True)
+if response and response.command == mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL and response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
+    print("Command accepted")
+else:
+    print("Command failed")
