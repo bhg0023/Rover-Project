6ce24254f5aac9f1ee8a0a89af672882d9601b4a
JuliaanB8 <92193516+JuliaanB8@users.noreply.github.com>
2025-11-05 19:55:39 -0600

    Update send_receive_test.py

--- Controls/send_receive_test.py
+++ Controls/send_receive_test.py
@@ -1,30 +1,11 @@
 from pymavlink import mavutil
 
 # Sets up communication interface between the PixHawk and the Jetson
-# Using UART with BAUD rate: 115200
-connection = mavutil.mavlink_connection('/dev/ttyTHS1', 115200)
+# Using UART with BAUD rate: 57600
+connection = mavutil.mavlink_connection('/dev/ttyTHS1', 57600)
 connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (connection.target_system, connection.target_component))
 
-
-message = connection.mav.command_long_encode(
-        connection.target_system,  # Target system ID
-        connection.target_component,  # Target component ID
-        mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,  # ID of command to send
-        0,  # Confirmation
-        mavutil.mavlink.MAVLINK_MSG_ID_BATTERY_STATUS,  # param1: Message ID to be streamed
-        1000000, # param2: Interval in microseconds
-        0,       # param3 (unused)
-        0,       # param4 (unused)
-        0,       # param5 (unused)
-        0,       # param5 (unused)
-        0        # param6 (unused)
-        )
-
-connection.mav.send(message)
-
-response = connection.recv_match(type='COMMAND_ACK', blocking=True)
-if response and response.command == mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL and response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
-    print("Command accepted")
-else:
-    print("Command failed")
+# Reading LiPo battery voltage
+msg = connection.recv_match(type='SYS_STATUS', blocking=True)
+print("Battery Voltage: " + str(msg.voltage_battery) +  ", " + "Battery Remaining: " + str(msg.battery_remaining) + "%")
